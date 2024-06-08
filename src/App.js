118fd0ae1a6038c7975f0b0f29c146698f447157
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-06-09 03:21:31 +0700

    Done Dark theme / Light theme mechanic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useContext } from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/layout/Home";
-
+import { ThemeContext } from "./ThemeContext";
 
 const App = () => {
+  const { isDark } = useContext(ThemeContext);
+  
   return (
-    <div className="App">
+    <div className={`App ${isDark ? "bg-dark" : ""}`}>
       <Router>
         <Navbar />
         <Home />
@@ -15,4 +17,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../ThemeContext";
+import "react-toggle/style.css";
+
+
+import Toggle from "react-toggle";
 
 const Navbar = () => {
+  const { isDark, setIsDark } = useContext(ThemeContext);
+
   return (
     <nav className="navbar bg-success">
       <h1>
         <i className="fab fa-github" /> GitHub Finder
       </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <ul style={{ display: "flex", listStyleType: "none"}}>
+          <li style={{ marginRight: "10px" }}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        
+        <Toggle
+          checked={isDark}
+          onChange={({ target }) => setIsDark(target.checked)}
+          icons={{ checked: "🌙", unchecked: "🔆" }}
+          aria-label="Dark mode toggle"
+        />
+      </div>
     </nav>
   );
 };
