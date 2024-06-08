f5da6f121cbe691fcaed7dcb220f5dad6ac990cd
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-06-09 02:05:23 +0700

    Create About and NotFound components

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="all-center">
+      <h1 className="x-large text-danger">404 - Page Not Found</h1>
+      <p className="lead">
+        Sorry, the page you are looking for doeas not exits.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound
